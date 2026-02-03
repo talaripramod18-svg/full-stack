eefc3ba33c7485751aed41d1748b7e866927a3f2
talaripramod18-svg <talaripramod18@gmail.com>
2026-02-03 23:44:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
 
-function bookNow() {
-    alert("Thank you for choosing Dream Tours & Travels! Our team will contact you soon 😊");
-}
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let errorMsg = document.getElementById("errorMsg");
+
+    if (name === "" || email === "" || message === "") {
+        errorMsg.textContent = "All fields are required!";
+        errorMsg.style.color = "red";
+        return;
+    }
+
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!email.match(emailPattern)) {
+        errorMsg.textContent = "Enter a valid email!";
+        errorMsg.style.color = "red";
+        return;
+    }
+
+    errorMsg.textContent = "Enquiry sent successfully! We will contact you 😊";
+    errorMsg.style.color = "green";
+    this.reset();
+});
